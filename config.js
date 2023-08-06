f96f4fa967f802d072fd31e470a77ba68c33aedf
Tudrug <108qwl@gmail.com>
2023-08-06 19:32:17 +0700

    Change local provider

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     GAS_THRESHOLD: 10 ** 15,
     VALUE_THRESHOLD: 10 * 10 ** 18,
     REMOTE_PROVIDER: `wss://mainnet.infura.io/ws/v3/9965e03532234a2eaa5bc3a9b6506390`,
-    LOCAL_PROVIDER: `wss://solemn-twilight-sky.discover.quiknode.pro/45306729a60ec2967c6fb992df74f97f573bcfb6/`,
+    LOCAL_PROVIDER: `wss://localhost:8546`,
     PAIRED_TABLE_SIZE: 10,
     REGULAR_TABLE_SIZE: 10
 }
